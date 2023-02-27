807e752ef218696212227af15186305a2bfdb735
Hau Nguyen <hau.nguyen@merkle.com>
2023-02-27 11:02:00 +0700

    update token

--- hydrogen.config.ts
+++ hydrogen.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     defaultCountryCode: 'US',
     defaultLanguageCode: 'EN',
     storeDomain: Oxygen.env.PUBLIC_STORE_DOMAIN,
-    storefrontToken: Oxygen.env.PUBLIC_STOREFRONT_API_TOKEN,
+    storefrontToken: 'b868d6440ac9ed42eee501e245a2e8ed',
     privateStorefrontToken: Oxygen.env.PRIVATE_STOREFRONT_API_TOKEN,
     storefrontApiVersion: '2022-07',
     storefrontId: Oxygen.env.PUBLIC_STOREFRONT_ID,
